91f08bba06822c890c2b1869380426a1d8e0ba48
carsonfrench <78115103+carsonfrench@users.noreply.github.com>
2023-02-15 00:34:34 +0000

    collatzCOunt

--- src/main/scala/collatz/Collatz.scala
+++ src/main/scala/collatz/Collatz.scala
@@ -1,1 +1,5 @@
-@main def collatz(n: Int) = {if n % 2 == 0 then n / 2 else 3 * n + 1}
+@main def collatz(n: Int) = {if (n%2 == 0) n/2 else (3*n + 1)}
+
+def collatzCount(n: Int) = {
+    if (n == 1) 0 else (1 + collatzCount(collatz(n)))
+}
